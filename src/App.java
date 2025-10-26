f87675365bb1112e05c14aa93f071368e59ce584
Felipe Vega Rivera <fvegarivera@gmail.com>
2025-10-25 19:43:10 -0500

    Modificacion 1

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola Knita!");
+        System.out.println("Hola Knita, te extraño mucho!!!");
     }
 }
 // 
